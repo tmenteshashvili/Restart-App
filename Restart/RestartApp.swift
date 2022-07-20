39949bcccb0114ea84c0f0ec54c3bb99f782c656
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2022-07-20 12:47:03 +0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Tako Menteshashvili on 20.07.22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
